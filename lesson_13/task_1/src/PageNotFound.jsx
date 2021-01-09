edec0094267a10683d3b7eac0308d79d1551faf6
mickel_jilenko <mickeljilenko@gmail.com>
2021-01-09 18:22:04 +0200

    r13 commit

---
+++ lesson_13/task_1/src/PageNotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function PageNotFound() {
+<div class="page__content">
+    <h1>😢</h1>
+    <Link to="/">Go home</Link>
+  </div>
+}
+export default PageNotFound;
